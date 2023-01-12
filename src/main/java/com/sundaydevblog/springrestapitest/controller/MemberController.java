d17ce3fe3a6863712b588f3700b7eee65c6a54c1
Smyejfatimaezzahra <fatimaezzahra.smyej@edu.uca.ma>
2023-01-12 23:04:19 +0000

    correcting

--- src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
+++ src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
@@ -72,7 +72,7 @@ public class MemberController {
 
     @GetMapping(value = "/hello")
     public String sayHi(){
-        return "Hello , jenkins for toggggday  test";
+        return "Hello , jenkins for today  test";
     }
 
 }
